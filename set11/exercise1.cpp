745930fa9495fb89c510992fe9fcda44f20a91df
Hollow Man <hollowman@opensuse.org>
2023-11-05 14:06:14 +0100

    Fix set11
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- set11/exercise1.cpp
+++ set11/exercise1.cpp
@@ -1,82 +1,83 @@
 #include <iostream>
-#include <vector>
+using namespace std;
 
-class TreeNode {
-   public:
-    int value;
+struct TreeNode {
+    int val;
     TreeNode* left;
     TreeNode* right;
 
-    TreeNode(int val) : value(val), left(nullptr), right(nullptr) {}
+    TreeNode(int value) : val(value), left(NULL), right(NULL) {}
 };
 
-TreeNode* fibonacci_tree(int n) {
-    if (n == 0 || n == 1) {
-        return new TreeNode(1);
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
     }
 
-    TreeNode* root = new TreeNode(n);
-    root->left = fibonacci_tree(n - 2);
-    root->right = fibonacci_tree(n - 1);
-    return root;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-void pre_order_traversal(TreeNode* node, std::vector<int>& result) {
-    if (node == nullptr) {
-        return;
+class FibonacciTree {
+public:
+    TreeNode* buildTree(int n) {
+        if (n <= 1) {
+            return new TreeNode(1);
+        }
+        TreeNode* root = new TreeNode(fibonacci(n + 1));
+        root->left = buildTree(n - 1);
+        root->right = buildTree(n - 2);
+        return root;
     }
-    result.push_back(node->value);
-    pre_order_traversal(node->left, result);
-    pre_order_traversal(node->right, result);
-}
 
-int tree_size(TreeNode* node) {
-    if (node == nullptr) {
-        return 0;
+    void preOrderTraversal(TreeNode* node) {
+        if (node != NULL) {
+            cout << node->val << " ";
+            preOrderTraversal(node->left);
+            preOrderTraversal(node->right);
+        }
     }
-    return 1 + tree_size(node->left) + tree_size(node->right);
-}
 
-int tree_depth(TreeNode* node) {
-    if (node == nullptr) {
-        return 0;
+    int calculateSize(TreeNode* node) {
+        if (node == NULL) {
+            return 0;
+        }
+        return 1 + calculateSize(node->left) + calculateSize(node->right);
     }
-    int left_depth = tree_depth(node->left);
-    int right_depth = tree_depth(node->right);
-    return 1 + std::max(left_depth, right_depth);
-}
 
-int count_leaves(TreeNode* node) {
-    if (node == nullptr) {
-        return 0;
+    int calculateDepth(TreeNode* node) {
+        if (node == NULL) {
+            return 0;
+        }
+        int leftDepth = calculateDepth(node->left);
+        int rightDepth = calculateDepth(node->right);
+        return 1 + max(leftDepth, rightDepth);
     }
-    if (node->left == nullptr && node->right == nullptr) {
-        return 1;
-    }
-    return count_leaves(node->left) + count_leaves(node->right);
-}
 
-void fibonacci_info(int n) {
-    TreeNode* root = fibonacci_tree(n);
-    std::vector<int> pre_order;
-    pre_order_traversal(root, pre_order);
-    int size = tree_size(root);
-    int depth = tree_depth(root);
-    int leaves = count_leaves(root);
-
-    std::cout << "Call tree in pre-order:";
-    for (int val : pre_order) {
-        std::cout << " " << val;
+    int calculateleafs(TreeNode* node) {
+        if (node == NULL) {
+            return 0;
+        }
+        if (node->left == NULL && node->right == NULL) {
+            return 1;
+        }
+        return calculateleafs(node->left) + calculateleafs(node->right);
     }
-    std::cout << std::endl;
-    std::cout << "Call tree size: " << size << std::endl;
-    std::cout << "Call tree depth: " << depth << std::endl;
-    std::cout << "Call tree leaves: " << leaves << std::endl;
-}
+};
 
 int main() {
     int n;
-    std::cin >> n;
-    fibonacci_info(n);
+    cin >> n;
+
+    FibonacciTree fibTree;
+    TreeNode* root = fibTree.buildTree(n);
+
+    cout << "Call tree in pre-order: ";
+    fibTree.preOrderTraversal(root);
+    cout << endl;
+
+    cout << "Call tree size: " << fibTree.calculateSize(root) << endl;
+    cout << "Call tree depth: " << fibTree.calculateDepth(root) << endl;
+    cout << "Call tree leafs: " << fibTree.calculateleafs(root) << endl;
+
     return 0;
 }
